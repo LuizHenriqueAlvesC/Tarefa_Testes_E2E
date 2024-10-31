eac7de3cb2abc3b65f09cd5e992478efcb8b03ad
Luiz <luiz980alves@gmail.com>
2024-10-31 19:23:42 -0300

    ajustando o codigo para não ter duplicação de elemento

--- cypress/e2e/Agenda_de_Contatos/adicaoContato.cy.js
+++ cypress/e2e/Agenda_de_Contatos/adicaoContato.cy.js
@@ -2,32 +2,44 @@
 
 describe('Teste para adicionar, editar e remover o contato', () => {
     beforeEach(() => {
-        cy.visit('https://agenda-contatos-react.vercel.app/')
-    })
-    
+        // Limpa o localStorage para resetar o estado da aplicação entre os testes
+        cy.visit('https://agenda-contatos-react.vercel.app/');
+        cy.clearLocalStorage();
+
+        // Aguarda e verifica se o botão de exclusão existe antes de tentar excluí-lo
+        cy.get('body').then(($body) => {
+            if ($body.find('.delete').length > 0) {
+                cy.get('.delete').each(($el) => {
+                    cy.wrap($el).click();
+                });
+            }
+        });
+    });
+
     it('Deve preencher as informações para adição de contato', () => {
-        cy.get('[type="text"]').type('Luiz Henrique')
-        cy.get('[type="email"]').type('Luiz980alves@gmail.com')
-        cy.get('[type="tel"]').type('12 12345678')
+        cy.get('[type="text"]').type('Luiz Henrique');
+        cy.get('[type="email"]').type('Luiz980alves@gmail.com');
+        cy.get('[type="tel"]').type('12 12345678');
         
-        cy.screenshot('Adicionando-teste')
+        cy.screenshot('Adicionando-teste');
         
-        cy.get('.adicionar').click()
-    })
+        cy.get('.adicionar').click();
+    });
 
     it('Deve editar as informações do contato', () => {
-        cy.get('.edit').last().click()
-        cy.get('[type="text"]').clear().type('Luiz Henrique Alves Cardoso')
-        cy.get('[type="email"]').clear().type('LuizInvestimentos980@gmail.com')
-        cy.get('[type="tel"]').clear().type('12 87654321')
+        cy.get('.edit').last().click();
+        cy.get('[type="text"]').clear().type('Luiz Henrique Alves Cardoso');
+        cy.get('[type="email"]').clear().type('LuizInvestimentos980@gmail.com');
+        cy.get('[type="tel"]').clear().type('12 87654321');
         
-        cy.screenshot('Editando-teste')
+        cy.screenshot('Editando-teste');
+        
+        cy.get('.alterar').click();
+    });
 
-        cy.get('.alterar').click()
-    })
     it('Deve excluir o contato', () => {
-        cy.get('.delete').last().click()
-
-        cy.screenshot('Excluindo-teste')
-    })
-})
+        cy.get('.delete').last().click();
+        
+        cy.screenshot('Excluindo-teste');
+    });
+});
